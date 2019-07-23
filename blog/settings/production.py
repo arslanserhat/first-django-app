1fc7d0c2873f095e54cdac716fff7e1662272222
serhat <serrhatarslan@gmail.com>
2019-07-23 17:59:08 +0300

    added hosts

--- blog/settings/production.py
+++ blog/settings/production.py
@@ -7,14 +7,13 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'pytest',
         'USER': 'serhat',
-        'PASSWORD': 'md21ty455',
-        'HOST': 'localhost',
-        'PORT': '',
+        'PASSWORD': 'md21ty455'
     }
 }
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
